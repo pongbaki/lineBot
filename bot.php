eb04b2c8ab3f6f8009e6d6cf6636d4fa19f28775
pongbaki <pong.supavita@gmail.com>
2017-10-06 22:20:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,8 +4,8 @@ header('Content-Type: application/json');
 $lat = 13.768384;
 $lon = 100.6144870;
 
-//$url = "http://samples.openweathermap.org/data/2.5/forecast/daily?lat=".$lat."&lon=".$lon."&cnt=10&appid=b1b15e88fa797225412429c1c50c122a1";
-$url = "http://samples.openweathermap.org/data/2.5/forecast?lat=".$lat."&lon=".$lon."&appid=b1b15e88fa797225412429c1c50c122a1"
+$url = "http://samples.openweathermap.org/data/2.5/forecast/daily?lat=".$lat."&lon=".$lon."&cnt=10&appid=b1b15e88fa797225412429c1c50c122a1";
+//$url = "http://samples.openweathermap.org/data/2.5/forecast?lat=".$lat."&lon=".$lon."&appid=b1b15e88fa797225412429c1c50c122a1"
 $contents = file_get_contents($url);
 $clima=json_decode($contents);
 
