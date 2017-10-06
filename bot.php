e71a0ff2c0d99b22e3de7524cec081bd1dd21e25
pongbaki <pong.supavita@gmail.com>
2017-10-06 20:44:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,18 @@
 <?php
-  echo "test";
-?>
+
+$url = "http://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=b1b15e88fa797225412429c1c50c122a1";
+
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
