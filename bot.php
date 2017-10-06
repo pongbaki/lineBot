a8fb43648651ef4cf24bad5bccbae3148e2181fc
pongbaki <pong.supavita@gmail.com>
2017-10-06 21:42:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,6 +13,7 @@ $icon=$clima->weather[0]->icon.".png";
 $today = date("F j, Y, g:i a");
 $cityname = $clima->main->city->name; 
 
+echo $clima->main . " \r\n" ;
 echo $cityname . " - " .$today . " \r\n";
 echo "Temp Max: " . $temp_max ." \r\n";
 echo "Temp Min: " . $temp_min ." \r\n";
