e11cc25c6ab47023550a0aaddc9ea8a09692cbae
pongbaki <pong.supavita@gmail.com>
2017-10-06 22:14:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,16 +6,9 @@ $url = "http://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139
 $contents = file_get_contents($url);
 $clima=json_decode($contents);
 
-$temp_max=$clima->main->temp_max;
-$temp_min=$clima->main->temp_min;
-$icon=$clima->weather[0]->icon.".png";
-//how get today date time PHP :P
-$today = date("F j, Y, g:i a");
-$cityname = $clima->city->name; 
+$cityname = $clima->city->name;
+$list = $clima->list;
 
-echo " test \r\n";
-echo $cityname . " - " .$today . " \r\n";
-echo "Temp Max: " . $temp_max ." \r\n";
-echo "Temp Min: " . $temp_min ." \r\n";
-
-
+foreach ($list as $value) {
+    echo "Date = " . date('m/d/Y',$value->dt) . " \r\n";
+}
