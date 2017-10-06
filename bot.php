3e73ee69b0a34737fc0fe48eeb1126558c84c624
pongbaki <pong.supavita@gmail.com>
2017-10-06 21:11:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,9 +14,8 @@ $result=curl_exec($ch);
 // Closing
 curl_close($ch);
 
-// Will dump a beauty json :3
-//echo json_encode($result);
+echo json_encode($result);
 
-//echo "\r\n\r\n";
+echo "\r\n\r\n";
 
-echo $result['list'][0];
+//echo $result['list'][0];
