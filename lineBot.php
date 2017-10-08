85245114efc8e0601ab36282deb7efe0cb94a390
pongbaki <pong.supavita@gmail.com>
2017-10-08 11:37:09 +0700

    Update lineBot.php

--- lineBot.php
+++ lineBot.php
@@ -55,7 +55,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "Next Saturday : " . date('d/m/Y',$list[$day_diff]->dt) . " \r\n" . "Weather : " . $weather ."\r\n" . $msg,
+				'text' => $dayWeather,
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
